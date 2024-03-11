c6cd65ea0b902703284cb031b7df732c0ff8efa5
charleshsc <1041807416@qq.com>
2024-03-11 13:05:31 +0800

    modify the script

--- commformer/scripts/train_smac.sh
+++ commformer/scripts/train_smac.sh
@@ -2,7 +2,7 @@
 env="StarCraft2"
 algo="commformer_dec"
 exp="single"
-seed=3
+seed=1
 name="CommFormer"
 
 map="3m"
@@ -13,7 +13,7 @@ echo "env is ${env}, map is ${map}, algo is ${algo}, exp is ${exp}, seed is ${se
 CUDA_VISIBLE_DEVICES=$1 python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
   --map_name ${map} --seed ${seed} --n_training_threads 16 --n_rollout_threads 32 --num_mini_batch 1 \
   --episode_length 100 --num_env_steps ${steps} --lr 5e-4 --ppo_epoch ${ppo_epochs} --clip_param ${ppo_clip} --save_interval 100000 \
-  --use_value_active_masks --use_eval --prefix_name ${name} --use_bilevel --use_bilevel
+  --use_value_active_masks --use_eval --prefix_name ${name} --use_bilevel
 
 map="8m"
 ppo_epochs=15
@@ -74,7 +74,7 @@ echo "env is ${env}, map is ${map}, algo is ${algo}, exp is ${exp}, seed is ${se
 CUDA_VISIBLE_DEVICES=$1 python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
   --map_name ${map} --seed ${seed} --n_training_threads 16 --n_rollout_threads 32 --num_mini_batch 1 \
   --episode_length 100 --num_env_steps ${steps} --lr 5e-4 --ppo_epoch ${ppo_epochs} --clip_param ${ppo_clip} --save_interval 100000 \
-  --use_value_active_masks --use_eval --prefix_name ${name} --use_bilevel --post_stable --alg_seed 1
+  --use_value_active_masks --use_eval --prefix_name ${name} --use_bilevel --post_stable
 
 map="8m_vs_9m"
 ppo_epochs=10
@@ -114,7 +114,7 @@ echo "env is ${env}, map is ${map}, algo is ${algo}, exp is ${exp}, seed is ${se
 CUDA_VISIBLE_DEVICES=$1 python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
   --map_name ${map} --seed ${seed} --n_training_threads 16 --n_rollout_threads 32 --num_mini_batch 1 \
   --episode_length 100 --num_env_steps ${steps} --lr 5e-4 --ppo_epoch ${ppo_epochs} --clip_param ${ppo_clip} --save_interval 100000 \
-  --use_value_active_masks --use_eval --prefix_name ${name} --use_bilevel --post_stable --alg_seed 1 --self_loop_add
+  --use_value_active_masks --use_eval --prefix_name ${name} --use_bilevel --post_stable --self_loop_add
 
 map="MMM2"
 ppo_epochs=10
@@ -124,7 +124,7 @@ echo "env is ${env}, map is ${map}, algo is ${algo}, exp is ${exp}, seed is ${se
 CUDA_VISIBLE_DEVICES=$1 python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
   --map_name ${map} --seed ${seed} --n_training_threads 16 --n_rollout_threads 32 --num_mini_batch 1 \
   --episode_length 100 --num_env_steps ${steps} --lr 5e-4 --ppo_epoch ${ppo_epochs} --clip_param ${ppo_clip} --save_interval 100000 \
-  --use_value_active_masks --use_eval --prefix_name CommFormer-v5-warm100 --use_bilevel --warmup 100 --alg_seed 1
+  --use_value_active_masks --use_eval --prefix_name ${name} --use_bilevel --warmup 100
 
 map="6h_vs_8z"
 ppo_epochs=15
@@ -134,7 +134,7 @@ echo "env is ${env}, map is ${map}, algo is ${algo}, exp is ${exp}, seed is ${se
 CUDA_VISIBLE_DEVICES=$1 python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
   --map_name ${map} --seed ${seed} --n_training_threads 16 --n_rollout_threads 32 --num_mini_batch 1 \
   --episode_length 100 --num_env_steps ${steps} --lr 5e-4 --ppo_epoch ${ppo_epochs} --clip_param ${ppo_clip} --save_interval 100000 \
-  --use_value_active_masks --use_eval --prefix_name ${name} --use_bilevel --post_stable --self_loop_add --alg_seed 1 --post_ratio 0.7
+  --use_value_active_masks --use_eval --prefix_name ${name} --use_bilevel --post_stable --self_loop_add --post_ratio 0.7
 
 map="3s5z_vs_3s6z"
 ppo_epochs=5
@@ -144,5 +144,5 @@ echo "env is ${env}, map is ${map}, algo is ${algo}, exp is ${exp}, seed is ${se
 CUDA_VISIBLE_DEVICES=$1 python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
   --map_name ${map} --seed ${seed} --n_training_threads 16 --n_rollout_threads 32 --num_mini_batch 1 \
   --episode_length 100 --num_env_steps ${steps} --lr 5e-4 --ppo_epoch ${ppo_epochs} --clip_param ${ppo_clip} --save_interval 100000 \
-  --use_value_active_masks --use_eval --prefix_name ${name} --use_bilevel --alg_seed 2 --post_stable --post_ratio 0.6 --self_loop_add
+  --use_value_active_masks --use_eval --prefix_name ${name} --use_bilevel --post_stable --self_loop_add --post_ratio 0.6
 
